0f3ae29beaccb482aa2e1646ec93428a48b48ae2
Mohammed Ameen <jegroamin@gmail.com>
2018-09-12 19:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
+
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   var result = template(issues);
@@ -12,6 +20,3 @@ Handlebars.registerHelper('comment_body', function() {
   }
 })
  
-function loadIssues() {
-  //...
-}
